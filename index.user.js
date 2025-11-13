170c0d1b7f249035595a0a4a6ab58f9de4af923d
Evan <168851101+susstevedev@users.noreply.github.com>
2025-11-13 17:54:53 -0600

    Update index.user.js

--- index.user.js
+++ index.user.js
@@ -3,9 +3,10 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://*.mecabricks.com/*
 // @grant       none
-// @version     0.1
-// @author      -
+// @version     0.2
+// @author      susstevedev
 // @description Removes user Grilledcheese from Mecabricks forum posts and comments with CSS.
+// @icon        https://www.google.com/s2/favicons?sz=64&domain=mecabricks.com
 // @require     https://cdn.jsdelivr.net/npm/js-cookie@3.0.5/dist/js.cookie.min.js
 // ==/UserScript==
 
